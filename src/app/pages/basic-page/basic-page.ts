972ca3fd76bbfff8d32c08aa0745ccba70a3c6ee
Rafael Botella <122386430+rafabo7@users.noreply.github.com>
2026-07-23 19:40:34 +0200

    add a series of basic pipes and numbers pipes basic builtin pipes for text and dates with dynamic clock add tipical numbers pipes

--- src/app/pages/basic-page/basic-page.ts
+++ src/app/pages/basic-page/basic-page.ts
@@ -1,9 +1,25 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.html',
 
 })
-export default class BasicPage {}
+export default class BasicPage {
+  nameLower = signal('rafael')
+  nameUpper = signal('RAFAEL')
+  fullName = signal('rAfAel boTELla')
+
+  customDate = signal( new Date() )
+
+  clockTickEffect = effect( (onCleanUp) => {
+    const interval = setInterval(() => {
+      this.customDate.set( new Date() )
+      console.log('tick');
+    }, 1000);
+
+    onCleanUp( () => clearInterval(interval) )
+  } )
+}
--- src/app/pages/numbers-page/numbers-page.ts
+++ src/app/pages/numbers-page/numbers-page.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-numbers-page',
-  imports: [],
+  imports: [DecimalPipe, PercentPipe, CurrencyPipe],
   templateUrl: './numbers-page.html',
 })
-export default class NumbersPage {}
+export default class NumbersPage {
+  totalSells = signal(4_987_456.5567)
+  percent = signal(0.4856)
+
+}
