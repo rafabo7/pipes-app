da4e6affcaad256174e56b9edf85ff7bc202a14c
Rafael Botella <122386430+rafabo7@users.noreply.github.com>
2026-07-27 16:31:09 +0200

    feat: exploring Content Projection and other pipes - add card component - use some pipes in uncommon-page

--- src/app/pages/uncommon-page/uncommon-page.ts
+++ src/app/pages/uncommon-page/uncommon-page.ts
@@ -1,8 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Card } from "../../components/card/card";
+import { I18nPluralPipe, I18nSelectPipe, SlicePipe } from '@angular/common';
+
+const client1 = {
+  name: 'Fernando',
+  gender:'male',
+  age: 30
+}
+const client2 = {
+  name: 'Amanda',
+  gender:'female',
+  age: 32
+}
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [],
+  imports: [Card, I18nSelectPipe, I18nPluralPipe, SlicePipe],
   templateUrl: './uncommon-page.html',
 })
-export default class UncommonPage {}
+export default class UncommonPage {
+  activeClient = signal(client1)
+
+  welcomeMap = {
+    male: 'Bienvenido',
+    female: 'Bienvenida'
+  }
+
+  //i18nPlural
+  clients = signal([
+    'Clarita',
+    'Mar',
+    'Alex',
+    'Kike',
+    'Javi',
+    'Victor',
+    'Marian',
+  ])
+
+  clientsMap = {
+    '=0': 'No hay nadie delante de tí en la cola, en breves momentos será tu turno',
+    '=1': 'Hay un cliente esperando delante de tí',
+    other: 'Hay # clientes esperando delante de tí',
+  }
+
+  changeClient() {
+    if (this.activeClient() === client1) {
+      this.activeClient.set(client2)
+      return
+    }
+    this.activeClient.set(client1)
+  }
+
+  deleteClient() {
+    this.clients.update( prev => prev.slice(1) )
+  }
+}
